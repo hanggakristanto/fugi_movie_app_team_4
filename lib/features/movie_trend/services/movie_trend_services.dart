0e54c260662e53085b6b95a0d7d8d018b934c1ec
Yonattan <34703013+lie16@users.noreply.github.com>
2022-08-17 21:09:14 +0700

    end of day

--- lib/features/movie_trend/services/movie_trend_services.dart
+++ lib/features/movie_trend/services/movie_trend_services.dart
@@ -1,19 +1,21 @@
+import 'dart:developer';
+
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
+import '../../../constant/global_variable.dart';
 import '../../../model/movie_trend.dart';
 
-
-class MovieTrendServices{
+class MovieTrendServices {
   Future<List<MovieTrend>> fetchAllTrend(BuildContext context) async {
     List<MovieTrend> movieTrend = [];
+    Uri uri = Uri.parse(url);
     try {
+      await http.get(uri);
+    } catch (e) {
+      log(e.toString());
+    } finally {}
 
-    }
-    catch {
-
-    }
     return movieTrend;
   }
-
 }

3181b98408eceb1cc939c0b0396dd95e945dd94d
Yonattan <yonattan_hope@yahoo.com>
2022-08-18 12:55:33 +0700

    finishing services without state management

--- lib/features/movie_trend/services/movie_trend_services.dart
+++ lib/features/movie_trend/services/movie_trend_services.dart
@@ -1,21 +1,41 @@
+import 'dart:convert';
 import 'dart:developer';
+import 'dart:html';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
+import '../../../constant/api_key.dart';
 import '../../../constant/global_variable.dart';
 import '../../../model/movie_trend.dart';
 
 class MovieTrendServices {
-  Future<List<MovieTrend>> fetchAllTrend(BuildContext context) async {
-    List<MovieTrend> movieTrend = [];
-    Uri uri = Uri.parse(url);
-    try {
-      await http.get(uri);
-    } catch (e) {
-      log(e.toString());
-    } finally {}
-
+  List<MovieTrend> parseData(String responseBody) {
+    final list = json.decode(responseBody).cast<Map<String, dynamic>>();
+    var movieTrend =
+        list.map<MovieTrend>((e) => MovieTrend.fromJson(e)).toList();
     return movieTrend;
+  }
+
+  Future<List<MovieTrend>> fetchAllTrend(BuildContext context, String mediaType,
+      String timeWindow, int page) async {
+    Uri uri =
+        Uri.parse('$url/$mediaType/$timeWindow?api_key=$apiKey&page=$page');
+    try {
+      http.Response res = await http.get(
+        uri,
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      );
+      if (res.statusCode == 200){
+        return compute(parseData, res.body);
+      } else {
+        throw Exception(res.statusCode);
+      }
+    } catch (e) {
+      throw Exception(e);
+    } 
   }
 }

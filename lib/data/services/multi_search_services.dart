038ed5a8a489f648c3f9529fac2713308f1e2ec4
Yonattan <yonattan_hope@yahoo.com>
2022-08-29 13:06:03 +0700

    create multi_services search and repository

---
+++ lib/data/services/multi_search_services.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../../constant/api_key.dart';
+import '../../constant/global_variable.dart';
+
+class MultiSearchServices{
+  Future<List<dynamic>> fetchQuery(
+    String language,
+    String query,
+    int page,
+    String includeAdult,
+    String region,
+  ) async {
+    Uri uri = Uri.parse(
+        '${GlobalVariable.url}/search/multi?api_key=${ApiKey.APIKEY}&page=$page&language=${language}&query=${query}&include_adult=${includeAdult}');
+      try {
+        final http.Response res = await http.get(
+        uri,
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      );
+      var data = jsonDecode(res.body)['results'];
+      return data as List<dynamic>;
+      } catch (e){
+        throw Exception(e);
+      }
+  }
+}

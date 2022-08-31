1649f52b50f3b77c0dc31b28560f8403c0310462
Yonattan <yonattan_hope@yahoo.com>
2022-08-31 14:52:44 +0700

    attemp with future builder

--- lib/presentation/widget/multi_search/multi_search_tile.dart
+++ lib/presentation/widget/multi_search/multi_search_tile.dart
@@ -10,8 +10,10 @@ Widget multiSearchTile(
   double height,
   double width,
 ) {
-  String? title =
-      (data.title != null) ? data.title : data.name;
+  String? title = (data.title != null) ? data.title : data.name;
+  String url = (data.poster_path != null)
+      ? '${GlobalVariable.urlImage}${data.poster_path}'
+      : GlobalVariable.urlNoImage;
   return InkWell(
     onTap: () {
       // TODO: add function later
@@ -20,30 +22,25 @@ Widget multiSearchTile(
       height: (MediaQuery.of(context).size.height / 3.9),
       width: (MediaQuery.of(context).size.width / 4),
       margin: const EdgeInsets.all(4.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Stack(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(
+            url,
+          ),
+        ),
+      ),
+      child: (data.poster_path == null)
+          ? Align(
             alignment: Alignment.bottomCenter,
-            children: [
-              Center(
-                child: (data.poster_path != null)
-                ? ImageLoading(
-                    url: '${GlobalVariable.urlImage}${data.poster_path}',
-                  )
-                : const Text('No Image Available', style: TextStyle(color: Colors.white),)
-              ),
-              Text(
+            child: Text(
                 "$title",
                 style: const TextStyle(
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-            ],
-          ),
-        ],
-      ),
+          )
+          : Container(),
     ),
   );
 }

e9f22eb4b67c3cb423c05adddd0819914245b3b6
hanggakristanto <hanggakristanto@gmail.com>
2022-09-01 09:18:10 +0700

    lates

--- lib/constant/api_key.dart
+++ lib/constant/api_key.dart
@@ -1,3 +1,3 @@
-class ApiKey{
-  static const String APIKEY = 'ISI_SENDIRI_API_KEYNYA';
+class ApiKey {
+  static const String APIKEY = 'c93f3d3d37caf9feda72e8c1f0dec1d3';
 }
---
+++ lib/provider/service_class.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+import 'data_class.dart';
+import 'data_model.dart';
+
+Future<DataModel?> getSinglePostData() async {
+  DataModel? result;
+  try {
+    final response = await http.get(
+      Uri.parse(
+          "https://api.themoviedb.org/3/movie/latest?api_key=c93f3d3d37caf9feda72e8c1f0dec1d3"),
+      headers: {
+        HttpHeaders.contentTypeHeader: "application/json",
+      },
+    );
+    print(response.body);
+    if (response.statusCode == 200) {
+      final item = json.decode(response.body);
+      result = DataModel.fromJson(item);
+    } else {
+      print("error");
+    }
+  } catch (e) {
+    log(e.toString());
+  }
+  return result;
+}
